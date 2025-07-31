db0e4254e0ef82011d20563062be91d75a6e1682
Alvaro Llorente <allorentee@gmail.com>
2025-07-31 17:53:20 +0200

    Actualizar enlaces de acceso en el encabezado y ajustar estilos en la sección de contenido para mejorar la presentación y la responsividad.

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -49,7 +49,10 @@ export default function Header() {
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+          <a
+            href="https://qr.menuvision.es/backoffice/auth/login"
+            className="text-sm/6 font-semibold text-gray-900"
+          >
             Acceso <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
@@ -90,7 +93,7 @@ export default function Header() {
               </div>
               <div className="py-6">
                 <a
-                  href="#"
+                  href="https://qr.menuvision.es/backoffice/auth/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Acceso
