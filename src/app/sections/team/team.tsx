c411ed4ab10a48b9ce3a8f38f5797144b7229b82
Alvaro Llorente <allorentee@gmail.com>
2025-09-06 14:35:23 +0200

    add linkedin profile

--- src/app/sections/team/team.tsx
+++ src/app/sections/team/team.tsx
@@ -20,7 +20,7 @@ const TEAM_MEMBERS = [
       "Desarrollo de web con amplia experiencia en la construcción de interfaces.",
     avatar: "https://avatars.githubusercontent.com/u/99037086?v=4",
     githubUrl: "https://github.com/Allorentee",
-    linkedinUrl: "www.linkedin.com/in/alvaro-llorente-0a2778150",
+    linkedinUrl: "https://www.linkedin.com/in/alvaro-llorente-0a2778150",
   },
 ];
 
